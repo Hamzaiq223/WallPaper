4b340ad3a5b5bc7f596f2772e48a4a8f3d93b6b9
mac <mac@macs-mbp-2.home>
2023-07-22 00:40:31 +0500

    View Image Implemented

--- app/src/main/java/com/example/wallpaper/Activities/ViewImage.java
+++ app/src/main/java/com/example/wallpaper/Activities/ViewImage.java
@@ -2,16 +2,23 @@ package com.example.wallpaper.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.wallpaper.R;
+import com.jsibbold.zoomage.ZoomageView;
 
 public class ViewImage extends AppCompatActivity {
 
     String image;
-    ImageView ivImage;
+    ImageView ivBack;
+
+    ZoomageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +26,18 @@ public class ViewImage extends AppCompatActivity {
         setContentView(R.layout.activity_view_image);
 
         ivImage = findViewById(R.id.ivImage);
-
+        ivBack = findViewById(R.id.ivBack);
         image = getIntent().getStringExtra("image");
+
+        ivBack.setOnClickListener(view -> {
+            onBackPressed();
+        });
 
         Glide.with(this)
                 .load(image)
                 .error(R.drawable.ic_launcher_background)
                 .into(ivImage);
+
     }
+
 }
